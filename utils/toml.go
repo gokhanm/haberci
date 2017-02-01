0567cbe775a67d49cf5b24c4afa6d60fab583bd3
Gokhan MANKARA <gokhan@mankara.org>
2017-02-01 14:15:22 +0300

    conf parser

---
+++ utils/toml.go
@@ -0,0 +1,47 @@
+package toml
+
+import "github.com/BurntSushi/toml"
+
+
+type MailConfig struct {
+    Server string   `toml:"server"`
+    Port int        `toml:"port"`
+    Username string `toml:"username"`
+    Password string `toml:"password"`
+    From string     `toml:"from"`
+    To string       `toml:"to"`
+    Subject string  `toml:"subject"`
+}
+
+type YtsConfig struct{
+    PageLimit string `toml:"pageLimit"`
+}
+
+type tConf struct {
+    Title string
+    Mail MailConfig
+    Yts YtsConfig
+}
+
+func Parse() (*tConf, error) {
+    tomlData := "/etc/haberci.toml"
+
+    var conf *tConf
+
+    if _, err := toml.DecodeFile(tomlData, &conf); err != nil {
+        return nil, err
+    }
+
+    return conf, nil
+}
+
+func Mail() MailConfig {
+    conf, _ := Parse()
+    return conf.Mail
+}
+
+func Yts() YtsConfig {
+    conf, _ := Parse()
+    return conf.Yts
+}
+
